293f41da0cdce7edf06fd2552cd4721647d4d676
Savel 40in <savel40in@MacBook-Air-Savel.local>
2026-04-29 08:42:26 +0200

    feat: implement keyboard controls for player

--- Painting.java
+++ Painting.java
@@ -1,22 +1,34 @@
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
-public class Painting extends JPanel{
+public class Painting extends JPanel implements KeyListener{
+
+    Map m = new Map(21);
+    Player p = new Player(1, 1);
 
     public Painting(Frame f){
-
+        addKeyListener(this);
+        setFocusable(true);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         
-        Map m = new Map(21);
-        
         m.drawMap(g);
-        
-        
-
+        p.drawPlayer(g);
     }
 
+    public void keyPressed(KeyEvent e) {
+        p.movement(e);
+        // p.printInfo();
+        repaint();
+    }
 
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
 }
---
+++ Player.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Player {
+   private int x;
+   private int y;
+
+   public Player(int x, int y) {
+      this.x = x;
+      this.y = y;
+   }
+
+   public void movement(KeyEvent e) {
+    int key = e.getKeyCode();
+    if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W) {
+         --this.y;
+      } else if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) {
+         ++this.y;
+      } else if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
+         --this.x;
+      } else if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
+         ++this.x;
+      }
+
+   }
+// prints out what the key listner does
+//    public void printInfo() {
+//       System.out.println("x:" + this.x + " y:" + this.y);
+//    }
+
+   public void drawPlayer(Graphics g) {
+		g.setColor(Color.GREEN);
+    	g.fillRect(x*30, y*30, 30, 30);
+	}
+}
